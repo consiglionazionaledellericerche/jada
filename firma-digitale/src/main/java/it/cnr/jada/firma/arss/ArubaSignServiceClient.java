8f8ca0253a820acee2ba75e621c0d42ec9ebfe91
francesco.uliana <francesco.uliana@b1829787-1b02-43dd-b0a5-8c37f85c9de8>
2014-03-10 11:00:37 +0000

    import ArubaSignService
    git-svn-id: http://150.146.206.176/svn/svn-jada/trunk@67 b1829787-1b02-43dd-b0a5-8c37f85c9de8

---
+++ firma-digitale/src/main/java/it/cnr/jada/firma/arss/ArubaSignServiceClient.java
@@ -0,0 +1,106 @@
+package it.cnr.jada.firma.arss;
+
+import it.cnr.jada.firma.arss.stub.ArubaSignService;
+import it.cnr.jada.firma.arss.stub.ArubaSignServiceService;
+import it.cnr.jada.firma.arss.stub.Auth;
+import it.cnr.jada.firma.arss.stub.DocumentType;
+import it.cnr.jada.firma.arss.stub.SignRequestV2;
+import it.cnr.jada.firma.arss.stub.SignReturnV2;
+import it.cnr.jada.firma.arss.stub.TypeOfTransportNotImplemented_Exception;
+import it.cnr.jada.firma.arss.stub.TypeTransport;
+import it.cnr.jada.firma.arss.stub.VerifyRequest;
+import it.cnr.jada.firma.arss.stub.VerifyReturn;
+
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class ArubaSignServiceClient {
+
+  private static final String CERT_ID = "arubaRemoteSignService.certId";
+  private static final String TYPE_OTP_AUTH = "arubaRemoteSignService.typeOtpAuth";
+
+  private static final String STATUS_OK = "OK";
+
+  private static final Logger LOGGER = Logger.getLogger(ArubaSignServiceClient.class);
+
+  private Properties props;
+
+  public byte[] pkcs7SignV2(String username, String password, String otp,
+      byte[] bytes) throws ArubaSignServiceException {
+    LOGGER.debug(username);
+    LOGGER.debug(otp);
+    Auth identity = getIdentity(username, password, otp);
+    return pkcs7SignV2(identity, bytes);
+  }
+
+  public byte[] verify(byte[] bytes) {
+    ArubaSignService service = new ArubaSignServiceService()
+        .getArubaSignServicePort();
+    VerifyRequest request = new VerifyRequest();
+    request.setBinaryinput(bytes);
+    request.setTransport(TypeTransport.BYNARYNET);
+    request.setType(DocumentType.PKCS_7);
+
+    VerifyReturn out = service.verify(request);
+    LOGGER.info(out.getStatus());
+    LOGGER.info(out.getDescription());
+    return out.getBinaryoutput();
+  }
+
+  public byte[] pkcs7SignV2(Auth identity, byte[] bytes)
+      throws ArubaSignServiceException {
+
+    LOGGER.debug(identity.getUser());
+
+    ArubaSignService service = new ArubaSignServiceService()
+        .getArubaSignServicePort();
+
+    LOGGER.debug("version " + service.getVersion());
+
+    try {
+      SignReturnV2 response = service.pkcs7SignV2(
+          getRequest(identity, bytes),
+          false,
+          false);
+
+      LOGGER.debug(response.getReturnCode() + " " + response.getStatus());
+
+      if (response.getStatus().equals(STATUS_OK)) {
+        return response.getBinaryoutput();
+      } else {
+        throw new ArubaSignServiceException("Server side error code "
+            + response.getReturnCode() + ", "
+            + response.getStatus());
+      }
+
+    } catch (TypeOfTransportNotImplemented_Exception e) {
+      throw new ArubaSignServiceException("error while invoking pkcs7SignV2", e);
+    }
+
+  }
+
+  private SignRequestV2 getRequest(Auth identity, byte[] bytes) {
+    SignRequestV2 request = new SignRequestV2();
+    request.setIdentity(identity);
+    request.setCertID(props.getProperty(CERT_ID));
+    request.setTransport(TypeTransport.BYNARYNET);
+    request.setBinaryinput(bytes);
+    return request;
+  }
+
+  private Auth getIdentity(String username, String password, String otp) {
+    Auth identity = new Auth();
+    identity.setUser(username);
+    identity.setUserPWD(password);
+    identity.setOtpPwd(otp);
+    identity.setTypeOtpAuth(props.getProperty(TYPE_OTP_AUTH));
+    return identity;
+  }
+
+
+  public void setProps(Properties props) {
+    this.props = props;
+  }
+
+}
---
+++ firma-digitale/src/test/java/it/cnr/jada/firma/arss/ArubaSignServiceClientTest.java
@@ -0,0 +1,49 @@
+package it.cnr.jada.firma.arss;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+public class ArubaSignServiceClientTest {
+
+
+  private static final String INPUT_FILE = "contenuto.txt";
+
+  private static final Logger LOGGER = Logger.getLogger(ArubaSignServiceClientTest.class);
+
+  private static final String USERNAME = "test_collaudo3";
+  private static final String PASSWORD = "password11";
+  private static final String OTP = "dspin";
+
+  @Test
+  public void testPkcs7SignV2() throws IOException, ArubaSignServiceException {
+    ArubaSignServiceClient client = new ArubaSignServiceClient();
+
+    InputStream is = ArubaSignServiceClientTest.class.getClassLoader()
+        .getResourceAsStream("aruba.properties");
+    Properties props = new Properties();
+    props.load(is);
+    client.setProps(props);
+
+    InputStream iss = ArubaSignServiceClientTest.class.getClassLoader()
+        .getResourceAsStream(INPUT_FILE);
+
+    byte[] bytes = IOUtils.toByteArray(iss);
+
+    byte[] content = client.pkcs7SignV2(USERNAME, PASSWORD, OTP, bytes);
+
+    assertTrue(content != null && content.length > 0);
+    LOGGER.info(new String(content));
+
+    byte[] out = client.verify(content);
+    LOGGER.info("messaggio decriptato");
+    LOGGER.info(new String(out));
+  }
+
+}
