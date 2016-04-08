f34c769b3ad013aba75702dec6a82ee9e6e2b63a
mspasiano <mspasiano@b1829787-1b02-43dd-b0a5-8c37f85c9de8>
2016-04-08 18:03:30 +0000

    Aggiornamento per la versione jboss 10
    git-svn-id: http://150.146.206.176/svn/svn-jada/trunk@140 b1829787-1b02-43dd-b0a5-8c37f85c9de8

--- src/main/java/it/cnr/jada/util/ejb/EJBCommonServices.java
+++ src/main/java/it/cnr/jada/util/ejb/EJBCommonServices.java
@@ -24,7 +24,9 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.text.Format;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.List;
 
 import javax.ejb.EJBException;
 import javax.ejb.EJBHome;
@@ -99,14 +101,30 @@ public class EJBCommonServices implements Serializable{
 		}
 	}
 
+	
 	public static final Object createRemoteEJB(String jndiName){
+		List<String> modules = Arrays.asList("jada", "sigla-ejb");
 		try {
 			if (earAppName==null)
 				loadEarAppName();
 			return getInitialContext().lookup(earAppName+"/"+jndiName+"/remote");
 		} catch (NamingException e) {
-			throw new EJBException(e);
+			Object obj = null;
+			for (String module : modules) {
+				try {
+					obj = createRemoteEJBInternal("/" + module +"/" + jndiName);
+				} catch (NamingException e1) {
+					logger.debug("NamingException", e1);
+				}
+			}
+			if (obj == null)
+				throw new EJBException(e);
+			return obj;
 		}
+	}
+	
+	private static final Object createRemoteEJBInternal(String jndiName) throws NamingException{
+		return getInitialContext().lookup("java:global/" + earAppName + jndiName);		
 	}
 
 	public static final TransactionalSessionImpl createEJB(it.cnr.jada.UserTransaction usertransaction, String jndiName) throws EJBException, RemoteException{
@@ -161,21 +179,31 @@ public class EJBCommonServices implements Serializable{
 		return traceUserConnection(usercontext, getConnection());
 	}
 
+
 	public static final DataSource getDatasource() throws SQLException, EJBException{
 		if (dataSourceName==null)
 			loadDataSourceName();
-		return getDatasource("java:"+dataSourceName);
+		try {
+			return getDatasource("java:"+dataSourceName);
+		} catch (EJBException e) {
+			try {
+				return getDatasource("java:/"+dataSourceName);
+			} catch (EJBException e1) {
+				throw e1;				
+			}
+		}
 	}
 
-	public static final DataSource getDatasource(String s) throws EJBException, SQLException{
+	public static final DataSource getDatasource(String s) throws EJBException, SQLException {
 		DataSource datasource = (DataSource)dataSources.get(s);
 		if(datasource == null)
 			try{
 				InitialContext initialcontext = getInitialContext();
 				dataSources.put(s, datasource = (DataSource)initialcontext.lookup(s));
-			}catch(NameNotFoundException namenotfoundexception){
-				throw new EJBException(namenotfoundexception);
-			}catch(NamingException namingexception){
+			} catch(NameNotFoundException namingexception){
+				dataSources = new Hashtable();
+				throw new EJBException(namingexception);
+			} catch(NamingException namingexception){
 				dataSources = new Hashtable();
 				throw new EJBException(namingexception);
 			}
