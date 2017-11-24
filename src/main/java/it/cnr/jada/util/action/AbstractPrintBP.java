ac3b220617ea27de133acc2bc33ed6d1f46e6a73
Marco Spasiano <marco.spasiano@cnr.it>
2017-11-24 18:56:15 +0100

    FIX doStampa in bootstrap

--- src/main/java/it/cnr/jada/util/action/AbstractPrintBP.java
+++ src/main/java/it/cnr/jada/util/action/AbstractPrintBP.java
@@ -7,6 +7,7 @@ import it.cnr.jada.util.jsp.Button;
 import java.io.IOException;
 import java.io.Serializable;
 import javax.servlet.ServletException;
+import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.PageContext;
 
 // Referenced classes of package it.cnr.jada.util.action:
@@ -23,6 +24,14 @@ public abstract class AbstractPrintBP extends FormBP
     protected AbstractPrintBP(String s)
     {
         super(s);
+    }
+
+    public void writeToolbarBootstrap(JspWriter jspwriter) throws IOException, ServletException {
+        Button abutton[] = new Button[2];
+        int i = 0;
+        abutton[i++] = new Button(Config.getHandler().getProperties(getClass()), "Toolbar.printWindow");
+        abutton[i++] = new Button(Config.getHandler().getProperties(getClass()), "Toolbar.close");
+        writeToolbar(jspwriter, abutton);
     }
 
     public Button[] createToolbar()
